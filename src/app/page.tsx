170e2f60d3315e088d34f7a116d5a6d169a8eae1
Ramitphi <18ucs144@lnmiit.ac.in>
2024-02-25 14:38:57 +0530

    public url updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { getFrameMetadata } from "@coinbase/onchainkit";
 import type { Metadata } from "next";
 
-const NEXT_PUBLIC_URL = "https://78ca-103-59-75-203.ngrok-free.app";
+const NEXT_PUBLIC_URL = "https://toshi-links.vercel.app";
 
 const frameMetadata = getFrameMetadata({
   buttons: [
